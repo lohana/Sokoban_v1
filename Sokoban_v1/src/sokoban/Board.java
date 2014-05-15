76191834065b9d0d13cbdaab9ed2cc71564fe9ab
Lohana Lema Moreta. <lohana.8@gmail.com>
2014-05-15 20:37:35 +0200

    IsGameEnd implementation

--- Sokoban_v1/src/sokoban/Board.java
+++ Sokoban_v1/src/sokoban/Board.java
@@ -1,9 +1,12 @@
 package sokoban;
 
+import java.util.ArrayList;
+
 // Class representing a figure Board for the Sokoban game.
 public class Board extends Figure{
 	
 	private Figure[][] figures;
+	private ArrayList<Position> finalPositions;
 	private Player player;
 
 	public Board(Position position, int rows, int cols) {
@@ -31,7 +34,15 @@ public class Board extends Figure{
 	
 	
 	public void setFigure(Figure figure){
+		if (figure instanceof Field){
+			if ( ((Field) figure).isFinal() ) 
+				finalPositions.add(figure.getPosition());
+		}
 		
 		figures[figure.position.getX()][figure.position.getY()] = figure;
+	}
+	
+	public ArrayList<Position> getFinalPositions(){
+		return finalPositions;
 	}
 }
